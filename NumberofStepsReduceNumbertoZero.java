2b50f2ffd4393ee2d664596270ee973d1b8808ba
MyElephantLover <jin.summer.1993@gmail.com>
2025-06-06 21:04:31 -0400

    commit

---
+++ NumberofStepsReduceNumbertoZero.java
@@ -0,0 +1,40 @@
+class Solution{
+    public int StepsOfReductions(int num){
+        // define a variable to calculate the steps
+        int counter = 0;
+
+        while (num > 0){
+            if (num % 2 == 0){
+                // replace num with the value out of num dividing by 2
+                num /= 2;
+            } else {
+                num --;
+            }
+            counter ++;
+        }
+        return counter;
+    }
+}
+
+
+// Bitwise Operations
+
+class Solution{
+    public int StepsOfReductions(int num){
+        int counter = 0;
+
+        while (num > 0){
+            // in Bitwise Operations, num: xxxxx0 & bitmask: 000001 
+            if (num % 1 == 0){
+                // >> means Bitwise shifting right in 1 position
+                num >> 1;
+            } else {
+                num --;
+            }
+
+            counter ++;
+        }
+
+        return counter ;;
+    }
+}
