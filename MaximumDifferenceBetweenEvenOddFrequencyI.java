d789c29fed8b5996e22e2488f1a2b3aa164ac257
MyElephantLover <jin.summer.1993@gmail.com>
2025-06-10 20:30:49 -0400

    commit

---
+++ MaximumDifferenceBetweenEvenOddFrequencyI.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+class Solution{
+    public int maxDifference(String s){
+        Map<character, Integer> c = new HashMap<>();
+
+        /*
+        HashMap.put() method add a key-value pair to the map or update values which associated with a 
+        a key-value pair
+        */
+
+        for (char ch: s.toCharArray()){
+            c.put(ch, c.getOrDefault(ch, 0) + 1);
+        };
+
+        // getOrDefault method retrieves a value from a key-value pair if the key exisits, or return a default 
+        // value that we specify
+
+        int maxOdd = 1;
+        int minEven = s.length();
+
+        for (int value : c.values()){ // set value as lopping the HashMap
+            if (value % 2 == 1){
+                maxOdd = Math.max(maxOdd, value);
+            } else {
+                minEven = Math.min(minEven, value);
+            }
+        }
+
+        return maxOdd - minEven;
+
+    }
+}
